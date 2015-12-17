d24bd05b18a634c336e8711b3e86ff31941eae36
Zoran Kocevski <zorankocevski@Zorans-MacBook-Air.local>
2015-12-17 19:43:10 +0100

    Add files for MyHood application.

---
+++ my-hood/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  my-hood
+//
+//  Created by Zoran Kocevski on 12/16/15.
+//  Copyright © 2015 Zoran Kocevski. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
+    }
+    
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+        postImg.image = DataService.instance.imageForPath(post.imagePath)
+    }
+}
